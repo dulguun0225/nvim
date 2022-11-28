314c31dbd370750792f6feea9d3b09b35cb2d5ad
Dulguun Otgon <dulguun0225@gmail.com>
2022-11-29 04:49:10 +0800

    init

---
+++ lua/user/options.lua
@@ -0,0 +1,9 @@
+vim.opt.listchars = { lead = '.' }
+vim.opt.list = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.number = true
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.fileencoding = "utf-8"
